dd4f3b832a7ee2de0fc7650602e0a8a610812965
Kaofang <benyapa_kh@cmu.ac.th>
2021-01-08 16:20:23 +0700

    update_fn

--- lab9_3.cpp
+++ lab9_3.cpp
@@ -1,3 +1,27 @@
-//cout << "Number of data = ";
-//cout << "Mean = ";
-//cout << "Standard deviation = ";
+#include <iostream>
+#include<fstream>
+#include<string>
+#include<cstdlib>
+#include <iomanip>
+#include<cmath>
+
+using namespace std;
+
+int main(){
+    int count = 0;
+    float sum = 0 ;
+    float r = 0 ;
+    string textline;
+    ifstream source("score.txt");
+    while(getline(source,textline))
+    {
+	    sum += atof(textline.c_str());
+	    r += atof(textline.c_str())*atof(textline.c_str()) ;
+	    count++;
+	}
+	
+    cout << "Number of data = "<< count;
+    cout << setprecision(3);
+    cout << "\nMean = "<< sum/count;
+    cout << "\nStandard deviation = "<<sqrt((r/count)-((sum/count)*(sum/count)));
+}
